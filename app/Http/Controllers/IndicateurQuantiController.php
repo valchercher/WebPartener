525ba04a2afd7b94b3203fcc2457efc6217925bf
valchercher <sarrpc1@gmail.com>
2024-03-06 11:03:37 +0000

    webPartener

---
+++ app/Http/Controllers/IndicateurQuantiController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\indicateurQuanti;
+use Illuminate\Http\Request;
+
+class IndicateurQuantiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(indicateurQuanti $indicateurQuanti)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(indicateurQuanti $indicateurQuanti)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, indicateurQuanti $indicateurQuanti)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(indicateurQuanti $indicateurQuanti)
+    {
+        //
+    }
+}
---
+++ app/Models/RA_Annee.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RA_Annee extends Model
+{
+    use HasFactory;
+}
